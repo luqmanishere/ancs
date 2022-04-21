e131174705c55afcc746b8ac24c6440acf095746
Ian Armour <ianmarmour@gmail.com>
2022-04-21 16:51:56 -0700

    Changed name of notification source gattnotification to just notification

--- src/characteristics/notification_source.rs
+++ src/characteristics/notification_source.rs
@@ -6,7 +6,7 @@ use nom::{multi::count, number::complete::le_u8, IResult};
 pub const NOTIFICATION_SOURCE_UUID: &str = "9FBF120D-6301-42D9-8C58-25E699A21DBD";
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct GattNotification {
+pub struct Notification {
     pub event_id: EventID,
     pub event_flags: EventFlag,
     pub category_id: CategoryID,
@@ -14,16 +14,16 @@ pub struct GattNotification {
     pub notification_uid: u32,
 }
 
-impl GattNotification {
-    /// Attempts to parse a `GattNotification` from a `&[u8]`
+impl Notification {
+    /// Attempts to parse a `Notification` from a `&[u8]`
     /// 
     /// # Examples
     /// ```
     /// # use ancs::attributes::category::CategoryID;
     /// # use ancs::attributes::event::EventFlag;
     /// # use ancs::attributes::event::EventID;
-    /// # use ancs::characteristics::notification_source::GattNotification;
-    /// let notification: GattNotification = GattNotification {
+    /// # use ancs::characteristics::notification_source::Notification;
+    /// let notification: Notification = Notification {
     ///     event_id: EventID::NotificationAdded,
     ///     event_flags: EventFlag::Silent,
     ///     category_id: CategoryID::Other,
@@ -33,7 +33,7 @@ impl GattNotification {
     ///
     /// let notification_bytes: [u8; 8] = notification.into();
     ///
-    /// let parsed_notification = GattNotification::parse(&notification_bytes).unwrap().1;
+    /// let parsed_notification = Notification::parse(&notification_bytes).unwrap().1;
     /// 
     /// assert_eq!(parsed_notification.event_id, EventID::NotificationAdded);
     /// assert_eq!(parsed_notification.event_flags, EventFlag::Silent);
@@ -41,7 +41,7 @@ impl GattNotification {
     /// assert_eq!(parsed_notification.category_count, 0);
     /// assert_eq!(parsed_notification.notification_uid, 4294967295_u32);
     /// ```
-    pub fn parse(i: &[u8]) -> IResult<&[u8], GattNotification> {
+    pub fn parse(i: &[u8]) -> IResult<&[u8], Notification> {
         let (i, event_id) = le_u8(i)?;
         let (i, event_flags) = le_u8(i)?;
         let (i, category_id) = le_u8(i)?;
@@ -50,7 +50,7 @@ impl GattNotification {
 
         Ok((
             i,
-            GattNotification {
+            Notification {
                 event_id: EventID::try_from(event_id).unwrap(),
                 event_flags: EventFlag::try_from(event_flags).unwrap(),
                 category_id: CategoryID::try_from(category_id).unwrap(),
@@ -61,16 +61,16 @@ impl GattNotification {
     }
 }
 
-impl From<GattNotification> for [u8; 8] {
-    /// Converts a `GattNotification` to a `[u8; 8]`
+impl From<Notification> for [u8; 8] {
+    /// Converts a `Notification` to a `[u8; 8]`
     /// 
     /// # Examples
     /// ```
     /// # use ancs::attributes::category::CategoryID;
     /// # use ancs::attributes::event::EventFlag;
     /// # use ancs::attributes::event::EventID;
-    /// # use ancs::characteristics::notification_source::GattNotification;
-    /// let notification: GattNotification = GattNotification {
+    /// # use ancs::characteristics::notification_source::Notification;
+    /// let notification: Notification = Notification {
     ///    event_id: EventID::NotificationAdded,
     ///    event_flags: EventFlag::Silent,
     ///    category_id: CategoryID::Other,
@@ -84,7 +84,7 @@ impl From<GattNotification> for [u8; 8] {
     ///assert_eq!(notification_bytes, expected_bytes)
     /// ```
 
-    fn from(original: GattNotification) -> [u8; 8] {
+    fn from(original: Notification) -> [u8; 8] {
         let mut bytes: [u8; 8] = [0; 8];
         let uid_as_u8 = original.notification_uid.to_le_bytes();
 
